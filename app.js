ab6ca87939017f0f32a287f661e39fa41f9340d1
Isaac-Chaves <ichavesgomez10@gmail.com>
2023-01-06 21:21:13 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require ("express");
+const app = express()
+const path = require("path")
+
+
+const publicFolderPath = path.resolve (__dirname, "./public");
+app.use ( express.static (publicFolderPath))
+
+//Levantar nuestro servidor
+const port = process.env.PORT || 3001;
+app.listen (port, () =>
+console.log('Servidor corriendo en el puerto $(port)'));
+
+app.get("/home", (req, res) =>{
+    let htmlPath = path.resolve(__dirname,"./views/home.html")
+    res.sendFile(htmlPath)
+}) 
